b38afdcbe8e478a8dae16da93b4fb8fcdac7e6b7
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2026-04-01 03:29:37 +0300

    Add message to FilesChanged

--- src/HotReload/Internal/FilesChanged.php
+++ src/HotReload/Internal/FilesChanged.php
@@ -5,8 +5,14 @@ declare(strict_types=1);
 namespace Thesis\HotReload\Internal;
 
 /**
- * Marker exception to differentiate cancellation.
+ * Marker exception to differentiate cancellation reason in hotReload().
  *
  * @internal
  */
-final class FilesChanged extends \RuntimeException {}
+final class FilesChanged extends \RuntimeException
+{
+    public function __construct()
+    {
+        parent::__construct('Task terminated: files changed');
+    }
+}

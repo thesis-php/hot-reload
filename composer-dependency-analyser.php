be2d1c60fa9b22b6b9713574820e92061212a621
Kenny1911 <o-muzyka@mail.ru>
2026-07-26 17:39:01 +0400

    Add ext-inotify to composer suggest

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,6 +3,10 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return new Configuration()
-    ->addPathToScan(__DIR__ . '/bin/hot-reload', isDev: false);
+    ->addPathToScan(__DIR__ . '/bin/hot-reload', isDev: false)
+    ->ignoreErrorsOnExtension('ext-inotify', [
+        ErrorType::DEV_DEPENDENCY_IN_PROD,
+    ]);
